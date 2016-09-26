a0a556d17babd89c924cdc61bb742bdaa839ed44
christian <christian.ambriz@gmail.com>
2016-09-26 00:28:21 -0700

    rearrange files and add comments

--- DataStructures/word_graph.rb
+++ DataStructures/word_graph.rb
@@ -1,4 +1,4 @@
-require './graph_node.rb'
+require '../DataStructures/graph_node.rb'
 
 class WordGraph
 
@@ -8,28 +8,28 @@ class WordGraph
     create_graph(stringSet)
   end 
 
-  def print_nodes_char
+  # prints word with length of secret word
+  def secret_word(length)
     @nodes.each do |node|
-      puts node.char
-    end
-  end
-
-  def printWordWithLength(length)
-    @nodes.each do |node|
-      findWord(node, '', length)
+      word  = find_word(node, '', length)
+      return word if word.length == length
     end
   end
 
   private
 
-  def findWord(node, curr_word, secret_word_length)
+  # depth first search to find secret word of secret word length
+  def find_word(node, curr_word, secret_word_length)
     new_word = curr_word + node.char
-    return puts new_word if(node.vertices.count == 0 && secret_word_length == new_word.length)
+    return new_word if(node.vertices.length == 0)
     node.vertices.each do |vertice|
-      findWord(vertice, new_word, secret_word_length)
+      word = find_word(vertice, new_word, secret_word_length)
+      return word if word.length == secret_word_length
     end
   end
 
+  # find graph node with char and creates a new new if
+  # no node is found
   def node_with_char(char)
     @nodes.each do |node|
       return node if(node.char == char)
@@ -39,11 +39,15 @@ class WordGraph
     return new_node
   end
 
+  # add vertice to new_node if curr_node 
+  # doesn't already have a vertice pointing to new_node
   def add_vertice(node, next_node)
     return node.contains?(next_node)
     node.add_node(next_node)
   end
 
+
+  # creates node graph with the give string set
   def create_graph(stringSet)
     stringSet.each do |string|
       string.split("").each_with_index do |char, i|
--- algorithms/secret_word.rb
+++ algorithms/secret_word.rb
@@ -1,16 +1,21 @@
 require 'set'
 require '../DataStructures/word_graph.rb'
 
+# this algorithms makes the assumption that secret word
+# contains all unique characters and enough hints
+# are given to find the correct secret word
 class SecretWord
 
   def initialize
-    secret_word = 'catfish'
-    hints = ['cat','tfi','fis','cht','ish']
+    # secret_word is 'catfish'
+    hints = ['atf', 'cat','tfi','cth','ish']
     wordLength = numberOfUniqueChars(hints)
     graph = WordGraph.new(hints)
-    graph.printWordWithLength(wordLength)
+    secret_word = graph.secret_word(wordLength) # returns catfish!!
+    puts secret_word
   end
 
+  # figures out unique characters to get length of secret word
   def numberOfUniqueChars(hints)
     set = Set.new
     hints.each do |string|
